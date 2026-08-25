ad5f63c80c23f7efae5b7b31b0211867f166f062
ankittiwari17042007-hars <ankittiwari17042007@gmail.com>
2026-08-25 22:33:29 +0530

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,7 @@
+function addProduct() {
+    alert("Add Product feature will be connected to the database soon.");
+}
+
+function editProduct(productName) {
+    alert("Editing product: " + productName);
+}
